8e5b1d62495e478beb5d7f22eeedda5c422542b0
bigmeco <bigmecoprog@gmail.com>
2019-09-05 16:49:06 +0300

    Local Server on Android

--- app/src/main/java/com/bigmeco/servandroid/MainActivity.kt
+++ app/src/main/java/com/bigmeco/servandroid/MainActivity.kt
@@ -3,6 +3,10 @@ package com.bigmeco.servandroid
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.github.sumimakito.awesomeqr.AwesomeQrRenderer
+import com.github.sumimakito.awesomeqr.RenderResult
+import com.github.sumimakito.awesomeqr.option.RenderOption
+import com.github.sumimakito.awesomeqr.option.color.Color
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
@@ -23,6 +27,39 @@ class MainActivity : AppCompatActivity() {
         getIPAddress {
             web.loadUrl("http://localhost:8080")
             textView.text = "$it:8080"
+
+            val color = Color()
+            color.light = 0xFFFbfff00.toInt() // for blank spaces
+            color.dark = 0xFFF00574B.toInt() // for non-blank spaces
+            color.background = 0xFFFFFFFFF.toInt() // for the background (will be overriden by background images, if set)
+
+            val renderOption =  RenderOption ()
+            renderOption.content =  "$it:8080"  // содержимое для кодирования
+            renderOption.size =  800  // размер окончательного изображения QR-кода
+            renderOption.borderWidth =  60  // ширина пустого пространства вокруг QR-кода
+            renderOption.color = color // установить цветовую палитру для QR- кода
+            try {
+                val result =  AwesomeQrRenderer .render (renderOption)
+                when {
+                    result.bitmap !=  null -> {
+                        // игра с растровым изображением
+                        imageView.setImageBitmap(result.bitmap)
+
+                    }
+                    result.type ==  RenderResult . OutputType . GIF -> {
+                        // Если ваш фон является GifBackground , изображение
+                        // будет сохранено в выходном файле, заданном в GifBackground
+                        //, и не будет возвращено здесь. В результате
+                        // result.bitmap будет нулевым.
+                    }
+                    else -> {
+                        // Ой, что-то пошло не так.
+                    }
+                }
+            } catch ( e :  Exception ) {
+                e.printStackTrace ()
+                // Упс, что-то пошло не так.
+            }
         }
 
     }
